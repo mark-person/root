ec75dd7e2835de319b7c6081023119ac6ae7ad28
dengxz <6882855@qq.com>
2018-10-31 19:55:18 +0800

    t

--- demo/src/main/java/com/ppx/cloud/demo/test/TestController.java
+++ demo/src/main/java/com/ppx/cloud/demo/test/TestController.java
@@ -5,7 +5,6 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.ModelAndView;
-import org.thymeleaf.expression.Maps;
 
 import com.ppx.cloud.common.contoller.ControllerReturn;
 
