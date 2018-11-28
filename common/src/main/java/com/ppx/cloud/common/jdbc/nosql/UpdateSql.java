a61d280861aeb6e89165fc56ca09f3c1bb444537
dengxz <6882855@qq.com>
2018-11-28 19:59:19 +0800

    t

--- common/src/main/java/com/ppx/cloud/common/jdbc/nosql/UpdateSql.java
+++ common/src/main/java/com/ppx/cloud/common/jdbc/nosql/UpdateSql.java
@@ -8,11 +8,13 @@ import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import org.springframework.util.StringUtils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mysql.cj.xdevapi.JsonValue;
+import com.mysql.cj.xdevapi.Session;
 
 /**
  * @author mark
@@ -125,23 +127,35 @@ public class UpdateSql {
 	public static void main(String[] args) {
 		// insert into map_uri_seq(uri_text) values('/test/test') on duplicate key update uri_times=uri_times+1
 		// 或使用insert into map_uri_seq(uri_tex) select '/test/test' from dual where not exists(select 1 from map_uri_seq where uri_text = '/test/test')
+//		
+//		UpdateSql u = new UpdateSql("stat_uri", "uri_seq", "(select uri_seq from map_uri_seq where uri_text = '/test/test')");
+//		u.set("lasted", "now()");
+//		u.inc("totalTime",100);
+//		u.max("maxTime", 100);
+//		u.inc("times", 1);
+//		u.set("avgTime", "totalTime/times");
+//		
+//		
+//		u.set("distribute", "'[1,0,0,0,0]'",  "JSON_SET(distribute, '$[0]', JSON_EXTRACT(distribute, '$[0]') + 1)");
+//		
+//		//u.addToSet("sql_set", "xxxxx");
+//		
+//		
+//		//u.max("maxTime", 100, "maxDetail", Map.of("mark", 1233));
+//		
+//		
+//		System.out.println("" + u);
 		
-		UpdateSql u = new UpdateSql("stat_uri", "uri_seq", "(select uri_seq from map_uri_seq where uri_text = '/test/test')");
-		u.set("lasted", "now()");
-		u.inc("totalTime",100);
-		u.max("maxTime", 100);
-		u.inc("times", 1);
-		u.set("avgTime", "totalTime/times");
+		long t = System.nanoTime();
+		try {
+			// 每隔n秒看看有没有连接
+			TimeUnit.SECONDS.sleep(3);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		//System.nanoTime() - t < 10 * 1e9
+		System.out.println("xxx:" + (10 * 1e2));
+		System.out.println("xxx:" + (System.nanoTime() - t > 3 * 1e9));
 		
-		
-		u.set("distribute", "'[1,0,0,0,0]'",  "JSON_SET(distribute, '$[0]', JSON_EXTRACT(distribute, '$[0]') + 1)");
-		
-		//u.addToSet("sql_set", "xxxxx");
-		
-		
-		//u.max("maxTime", 100, "maxDetail", Map.of("mark", 1233));
-		
-		
-		System.out.println("" + u);
 	}
 }
