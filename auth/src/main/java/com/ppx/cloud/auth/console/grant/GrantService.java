fe662ed338c5ad80861149783accfc80189bf79e
mark <13602493280@139.com>
2018-12-17 21:50:14 +0800

    t

--- auth/src/main/java/com/ppx/cloud/auth/console/grant/GrantService.java
+++
@@ -1,9 +0,0 @@
-package com.ppx.cloud.auth.console.grant;
-
-import java.util.List;
-
-public interface GrantService {
-    
-    List<Integer> getGrantResIds(Integer accountId);
-    
-}
--- auth/src/main/java/com/ppx/cloud/auth/console/res/ResService.java
+++
@@ -1,7 +0,0 @@
-package com.ppx.cloud.auth.console.res;
-
-import java.util.Map;
-
-public interface ResService {
-    Map<String, Object> getResource();
-}   
