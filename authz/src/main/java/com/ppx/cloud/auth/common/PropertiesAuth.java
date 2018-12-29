c54241c9376d1251c3e165b59652349a3948113b
dengxz <6882855@qq.com>
2018-12-29 18:34:21 +0800

    t

---
+++ authz/src/main/java/com/ppx/cloud/auth/common/PropertiesAuth.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.ppx.cloud.auth.common;
+
+/**
+ * #由数据库初始化
+ * @author mark
+ * @date 2018年12月29日
+ */
+public class PropertiesAuth {
+
+	public static String adminPassword;
+	
+	public static String jwtPassword;
+	
+	public static String jwtValidateSecond;
+}
