3629e98d756ff005b5ef5ebc3e2cc2b524b956c2
dengxz <6882855@qq.com>
2019-01-08 18:36:40 +0800

    t

---
+++ common/src/main/java/com/ppx/cloud/config/pojo/Todo.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.ppx.cloud.config.pojo;
+
+/**
+ * @author mark
+ * @date 2019年1月8日
+ */
+public class Todo {
+
+	/**
+	 * 
+	 * 
+	 */
+
+	// table 
+	// service_id, 
+	
+	
+}
